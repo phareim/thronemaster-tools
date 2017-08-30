1e94dcd378bb27cbf556e649cd7a37d5e003a660
Petter Hareim <petter.hareim@bouvet.no>
2017-08-30 11:17:52 +0200

    better print from command-line, proper index.js-file.

--- bin/run.js
+++ bin/run.js
@@ -1,21 +1,34 @@
 #!/usr/bin/env node
-var thronemaster = require('../lib/index.js');
+var colors = require('colors');
+var thronemaster = require('../index.js');
 var args = process.argv.splice(process.execArgv.length + 2);
 var gameId = (args[0] ? args[0] : false);
 var moveId = (args[1] ? args[1] : false);
+
+function printStatus(s) {
+    if (s) {
+        console.log(colors.inverse(' Turn ' + s.turn + ' (move ' + s.id + '): ' + s.player.bold + ' ' + s.gamePhase.toLowerCase() + ' '));
+        console.log('' + s.logEntry);
+        console.log('' + s.date.grey + '\n');
+    }
+}
+
 if (gameId) {
     thronemaster.getLog(gameId, function (status) {
         var l = status.length;
-        if (!moveId) {
-            console.log('Status last move of game: ' + gameId);
-            console.log(status.pop());
-            console.log('--> thronemaster ' + gameId + ' [moveNumber] for other rounds. (' + l + ')');
+        if (moveId === 'ALL') {
+            status.forEach(function (t) {
+                printStatus(t);
+            });
+        } else if (moveId) {
+            printStatus(status[moveId - 1]);
         } else {
-            console.log('Status move number ' + moveId + ' of game: ' + gameId);
-            console.log(status[moveId - 1]);
+            var helpMessage = 'For other moves --> thronemaster ' + gameId + ' [move] (move: 1 - ' + l + ' | ALL)';
+            printStatus(status.pop());
+            console.log(helpMessage.grey);
         }
     });
 }
 else {
-    console.log('Usage: > thronemaster [game-id]');
+    console.log('Usage:'.bold + ' > thronemaster [game-id]'.grey);
 }
